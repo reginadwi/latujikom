56cc40049ef14dc96bd9292ff61c0c2dab4ff808
reginadwi <rdwiapriliyanti@gmail.com>
2019-08-01 10:36:03 +0700

    mengubah parent

--- app/Pasien.php
+++ app/Pasien.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pasien extends Model
 {
-    //
+    protected $fillabel =['nm_pasien','jk','agama',
+                        'alamat','tgl_lhr','usia','no_tlp','nm_kk','hub_kel'];
+public $timestamps = true;
+
+public function Pasien()
+{
+return $this->belongsTo('App\Pasien','nm_pasien');
+
+}
 }
--- app/RekamMedis.php
+++ app/RekamMedis.php
@@ -6,11 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class RekamMedis extends Model
 {
-   protected $fillabel =['no_RM','kd_tindakan','kd_obat','kd_user','no_pasien','diagnosa','resep','keluhan','tgl_pemeriksaan','ket'];
+   protected $fillabel =['no_RM','kd_tindakan','kd_obat','kd_user','no_pasien',
+                        'diagnosa','resep','keluhan','tgl_pemeriksaan','ket'];
    public $timestamps = true;
 
-   public function RekamMedis()
+   public function Pasien()
    {
-        return $this->belongsTo('App\RekamMedis','no_RM');
+        return $this->hasMany('App\Pasien','no_RM');
+
    }
+   public function Dokter()
+   {
+        return $this->belongsTo('App\Dokter','kd_dokter');
+
+   }
+   
+
 }
